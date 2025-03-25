06fe3d40bec7cd9cf3d3f36219e674d58f658300
jacobobendrado <jacobobendrado@gmail.com>
2025-03-25 12:09:54 -0400

    Initial syscall work

---
+++ ShompOS/mnt/inc/syscalls.c
@@ -0,0 +1,16 @@
+
+#include <stdint.h>
+
+extern uint32_t do_syscall(const uint32_t eax, const uint32_t ebx, const uint32_t ecx, const uint32_t edx, const uint32_t esi, const uint32_t edi, const uint32_t ebp);
+
+void exit(int32_t error_code) {
+    do_syscall(1, error_code, 0, 0, 0, 0, 0);
+}
+
+uint32_t read(uint32_t fd, char *buf, uint32_t count) {
+    do_syscall(3, fd, buf, count, 0, 0, 0);
+}
+
+uint32_t write(uint32_t fd, const char *buf, uint32_t count) {
+    do_syscall(4, fd, buf, count, 0, 0, 0);
+}
